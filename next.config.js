a243e0ef6aec7e8164adfb6d715e694b1fcb2635
erickeira <erick.almeida.ricardo@gmail.com>
2023-02-09 08:26:10 -0400

    hotsite info

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // experimental: {
-  //  runtime: 'experimental-edge',
-  //  appDir: true,
-  // },
-
+  experimental: {
+   runtime: 'experimental-edge',
+   appDir: true,
+  },
   reactStrictMode: true,
   swcMinify: false,
   images: {
