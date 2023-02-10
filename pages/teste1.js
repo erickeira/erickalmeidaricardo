dafd07c9eddc2c62c293917d651d6ae5dffd54de
erickeira <erick.almeida.ricardo@gmail.com>
2023-02-10 11:45:15 -0400

    teste

--- pages/teste1.js
+++ pages/teste1.js
@@ -1,5 +1,35 @@
-export default function Home(){
+import { useRouter } from 'next/router'
+
+export default function Home({list}){
+    const router = useRouter()
     return(
-        <div>oi</div>
+        <div onClick={() => router.push('teste2')}>
+            {list.destaques[0].id}
+        </div>
     )
+}
+
+export async function getServerSideProps(){
+    const apiId = "992";
+    const apiUrl = "https://dev.infoimoveis.com.br/webservice/hotsites.php";
+
+    const corpo = await JSON.stringify( {
+        acoes: [                        
+            { metodo: "destaques", params: [ { resultados: "4" }] },
+            { metodo: "ultimasnoticias", params: [ { resultados: "4" }] },
+        ], id: apiId
+    });
+    const resposta = await fetch(
+        apiUrl,
+        {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: corpo
+        }
+    )
+
+    const list = await resposta.json()
+    return{
+        props : { list }
+    }
 }
