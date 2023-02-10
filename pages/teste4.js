6c29c5709effe8d6c488c4bf6b5778292e7e1b86
erickeira <erick.almeida.ricardo@gmail.com>
2023-02-10 14:11:53 -0400

    teste requisicao iguala da documentacao

--- pages/teste4.js
+++ pages/teste4.js
@@ -1,42 +1,46 @@
 import { useRouter } from 'next/router'
 
-export default function Home({list}){
+export default function Home({ runtime,data }){
     const router = useRouter()
+    console.log(data)
     return(
         <div onClick={() => 
             router.push({
                 pathname: `/teste5`,
                 query: {  }
-              }, 
-              undefined, { shallow: true }
+              }
             )
         }>
-            {list.destaques[0].id}
+            {data.destaques[0].id}
         </div>
     )
 }
 
-export async function getServerSideProps(){
+export const getServerSideProps = async () => {
     const apiId = "992";
     const apiUrl = "https://dev.infoimoveis.com.br/webservice/hotsites.php";
-
     const corpo = await JSON.stringify( {
         acoes: [                        
             { metodo: "destaques", params: [ { resultados: "4" }] },
             { metodo: "ultimasnoticias", params: [ { resultados: "4" }] },
         ], id: apiId
     });
-    const resposta = await fetch(
-        apiUrl,
-        {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-            body: corpo
-        }
-    )
 
-    const list = await resposta.json()
-    return{
-        props : { list }
-    }
-}
+    return {
+      props: {
+        runtime: process.env.NEXT_RUNTIME,
+        data: await fetch(
+            apiUrl,
+            {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+                body: corpo
+            }
+        ).then((response) =>
+          response.json()
+        ),
+      },
+    };
+
+};
+
--- pages/teste5.js
+++ pages/teste5.js
@@ -1,7 +1,8 @@
 import { useRouter } from 'next/router'
 
-export default function Home({list}){
+export default function Home({ runtime,data }){
     const router = useRouter()
+    console.log(data)
     return(
         <div onClick={() => 
             router.push({
@@ -10,32 +11,36 @@ export default function Home({list}){
               }
             )
         }>
-            {list.destaques[0].id}
+            {data.destaques[0].id}
         </div>
     )
 }
 
-export async function getServerSideProps(){
+export const getServerSideProps = async () => {
     const apiId = "992";
     const apiUrl = "https://dev.infoimoveis.com.br/webservice/hotsites.php";
-
     const corpo = await JSON.stringify( {
         acoes: [                        
             { metodo: "destaques", params: [ { resultados: "4" }] },
             { metodo: "ultimasnoticias", params: [ { resultados: "4" }] },
         ], id: apiId
     });
-    const resposta = await fetch(
-        apiUrl,
-        {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-            body: corpo
-        }
-    )
 
-    const list = await resposta.json()
-    return{
-        props : { list }
-    }
-}
+    return {
+      props: {
+        runtime: process.env.NEXT_RUNTIME,
+        data: await fetch(
+            apiUrl,
+            {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+                body: corpo
+            }
+        ).then((response) =>
+          response.json()
+        ),
+      },
+    };
+
+};
+
