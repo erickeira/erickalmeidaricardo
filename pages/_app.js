ba42198044648a65e00f4661b9a3ec002ef8f1a7
erickeira <erick.almeida.ricardo@gmail.com>
2023-02-10 15:39:52 -0400

    topo

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import AuthProvider from '../context';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from '../components/Header';
+import Header from '../components/Topo';
 import Footer from '../components/Footer';
 
 
@@ -9,7 +9,7 @@ import Footer from '../components/Footer';
 export default function App({ Component, pageProps }) {
   return (
     <AuthProvider>
-      <Header/>
+      <Topo/>
       <Component {...pageProps} />
       <Footer/>
     </AuthProvider>
