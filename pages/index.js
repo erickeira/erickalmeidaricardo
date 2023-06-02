c5159a1ebd5a5f268f3c2d520081800a1627e287
erickeira <erick.almeida.ricardo@gmail.com>
2023-06-02 17:54:09 -0400

    packge particles

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>EricK Almeida Ricardo</title>
+        <title>Erick Almeida Ricardo</title>
         <meta name="description" content="FullStack Developer | ReactJS | ReactNative | Javascript | PHP | SQL" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
